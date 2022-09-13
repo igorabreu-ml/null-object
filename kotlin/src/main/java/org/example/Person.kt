62e03de4e9a20a84dad0792c2d8afc37cd0adfb9
igoabreu <igor.abreu@mercadolivre.com>
2022-09-13 15:13:16 -0300

    Kotlin

--- kotlin/src/main/java/org/example/Person.kt
+++ kotlin/src/main/java/org/example/Person.kt
@@ -1,6 +1,6 @@
 package org.example
 
-class Person(val name: String, val children: List<Person>?, val generation: Int?) {
+class Person(val name: String, val children: List<Person>?, val generation: Int) {
 
     fun print() {
         println("Name: $name, Generation: $generation")
